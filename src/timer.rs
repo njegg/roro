85a9199d1ca3ea41c8bbdc8eadeb83b88d9faff8
njegg <njegomir15@gmail.com>
2023-07-09 10:23:30 +0200

    fix: make timer respond better to pause & play

--- src/timer.rs
+++ src/timer.rs
@@ -24,27 +24,30 @@ enum TimerState {
 
 #[allow(dead_code)]
 struct Timer {
-    is_playing: bool,
-    state: TimerState,
     time_left: Duration,
-    pomo: u32,
+    pomo: u64,
 
-    work_time: u32,
-    break_time: u32,
-    long_break_time: u32,
+    work_time: u64,
+    break_time: u64,
+    long_break_time: u64,
 
-    long_break_interval: u32,
+    long_break_interval: u64,
+
+    state: TimerState,
+    is_playing: bool,
 }
 
 impl Timer {
     fn default_state() -> Timer {
+        let work_time: u64 = 25;
+
         return Timer {
             is_playing: false,
             state: TimerState::Work,
-            time_left: Duration::from_secs(10),
+            time_left: Duration::from_secs(work_time * 60),
             pomo: 1,
 
-            work_time: 10,
+            work_time,
             break_time: 5,
             long_break_time: 15,
             
@@ -64,21 +67,10 @@ pub fn spawn_timer_thread(rx: Receiver<TimerCommand>, tx_ui: Sender<UiMessage>) 
 
         // TODO is zero => stopped, add Reset state
         while !timer.time_left.is_zero() {
-            if !timer.is_playing {
-                loop {
-                    match rx.recv().unwrap() {
-                        Play => { timer.is_playing = true; break },
-
-                        Exit => break,
-                        _ => (), // send to ui to update state on Next and Prev
-                    }
-                }
-            } else {
-                tx_ui.send(UiMessage::Time(timer.time_left)).unwrap();
-
-                timer.time_left -= SECOND;
+            if timer.is_playing {
                 std::thread::sleep(SECOND);
 
+                // Check for commands without blocking
                 match rx.try_recv() {
                     Ok(command) => match (&timer.state, &command) {
                         (_, Play) => timer.is_playing = !timer.is_playing,
@@ -88,6 +80,23 @@ pub fn spawn_timer_thread(rx: Receiver<TimerCommand>, tx_ui: Sender<UiMessage>) 
                     Err(why) => match why {
                         mpsc::TryRecvError::Empty => (),
                         mpsc::TryRecvError::Disconnected => panic!("{}", why),
+                    }
+                }
+
+                // If pause was hit while timer was sleeping dont decrease time
+                if timer.is_playing {
+                    timer.time_left -= SECOND;
+                }
+
+                tx_ui.send(UiMessage::Time(timer.time_left)).unwrap();
+            } else {
+                loop {
+                    // Wait for command
+                    match rx.recv().unwrap() {
+                        Play => { timer.is_playing = true; break },
+
+                        Exit => break,
+                        _ => (), // send ui to update state on Next and Prev
                     }
                 }
             }
